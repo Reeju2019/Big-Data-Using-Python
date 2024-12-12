b767677d1d0b8a9741b03b8435c2798075a5ec7e
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2024-12-12 16:22:13 +0100

    cleaning code

--- file_handleing.py
+++ file_handleing.py
@@ -1,30 +1,44 @@
 import csv
 
-class FileManager():
-    def __init__(self, filename, mode):
-        self.filename = filename
-        self.mode = mode
-        self.file = None
+# class FileManager():
+#     def __init__(self, filename, mode):
+#         self.filename = filename
+#         self.mode = mode
+#         self.file = None
 
-    def __enter__(self):
-        self.file = open(self.filename, self.mode, newline='')  # Added `newline=''` for CSV compatibility
-        return self.file
+#     def __enter__(self):
+#         self.file = open(self.filename, self.mode, newline='')  # Added `newline=''` for CSV compatibility
+#         return self.file
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.file.close()
+#     def __exit__(self, exc_type, exc_value, exc_traceback):
+#         self.file.close()
 
-# Reading from a CSV file and writing its content to a new CSV file
+# # Reading from a CSV file and writing its content to a new CSV file
+# input_file = 'organizations-1000.csv'
+# output_file = 'output.csv'
+
+# # Read the content of the input CSV
+# with FileManager(input_file, 'r') as file:
+#     reader = csv.reader(file)
+#     rows = list(reader)  # Store rows in a list
+
+# # Write the content to the output CSV
+# with FileManager(output_file, 'w') as outfile:
+#     writer = csv.writer(outfile)
+#     writer.writerows(rows)
+
+# print("CSV content copied successfully.")
+
+
+import csv
+
 input_file = 'organizations-1000.csv'
 output_file = 'output.csv'
 
-# Read the content of the input CSV
-with FileManager(input_file, 'r') as file:
-    reader = csv.reader(file)
-    rows = list(reader)  # Store rows in a list
-
-# Write the content to the output CSV
-with FileManager(output_file, 'w') as outfile:
+with open(input_file, 'r', newline='') as infile, open(output_file, 'w', newline='') as outfile:
+    reader = csv.reader(infile)
     writer = csv.writer(outfile)
-    writer.writerows(rows)
+    writer.writerows(reader)
 
 print("CSV content copied successfully.")
+
